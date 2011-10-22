79ecf507d57912165b69a41d365688589cf6d53c
BrandonMathis <Brandon@KeysetTS.com>
2011-10-22 14:26:04 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodTrucker::Application.config.secret_token = 'f27909cbff504e5f48f12aca2add886327552a5551ec15c3d08c53808fb21547c5aa6587aba69fc28d470c78d3f2335c768cf5a1a4a36e4a3002c6c80b85981e'
